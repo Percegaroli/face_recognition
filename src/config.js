52be3c7090d5140dd30fd944bbd5457160b98a21
Percegaroli <bruno.percegaroli@gmail.com>
2021-08-08 00:39:11 -0300

    initial commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  foldersPath: {
+    output: './src/output',
+    queryImage: './src/queryImage',
+    referenceImages: './src/referenceImages'
+  },
+  resultBox: {
+    matchColor: 'rgba(78, 205, 196, 1)',
+    unknownPersonColor: 'rgba(192, 57, 43, 1)' 
+  },
+  referenceName: 'Keanu'
+}
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const faceapi= require('@vladmandic/face-api/dist/face-api.node-cpu.js');
+const canvas = require('canvas');
+const { readdir, writeFile } = require('fs/promises')
+const { foldersPath, referenceName, resultBox } = require('./config');
+
+const { Canvas, Image, ImageData } = canvas
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+
+const initializeNeuralModels = async () => {
+  const modelsFolderPath = './src/model';
+  await Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromDisk(modelsFolderPath),
+    faceapi.nets.tinyFaceDetector.loadFromDisk(modelsFolderPath),
+    faceapi.nets.faceLandmark68Net.loadFromDisk(modelsFolderPath),
+    faceapi.nets.faceRecognitionNet.loadFromDisk(modelsFolderPath)
+  ]);
+}
+
+const loadReferenceImages = async () => {
+  const referenceFolderPath = foldersPath.referenceImages;
+  const referenceImagesFolder = await readdir(referenceFolderPath);
+  return Promise.all(referenceImagesFolder.map(reference => canvas.loadImage(`${referenceFolderPath}/${reference}`)));
+}
+
+const loadQueryImage = async () => {
+  const queryImageFolderPath = foldersPath.queryImage;
+  const queryImagesFolder = await readdir(queryImageFolderPath);
+  return canvas.loadImage(`${queryImageFolderPath}/${queryImagesFolder[0]}`);
+}
+
+const recognizeFaces = async () => {
+  const [ referenceImages, queryImage ] = await Promise.all([loadReferenceImages(), loadQueryImage()]);
+  const queryImageDescriptorTask = faceapi.detectAllFaces(queryImage).withFaceLandmarks().withFaceDescriptors();
+  const referenceImagesDescriptors = await createReferenceLabeledDescriptors(referenceImages);
+  const faceMatcher = new faceapi.FaceMatcher(referenceImagesDescriptors);
+  drawResults(faceMatcher, await queryImageDescriptorTask, queryImage);
+}
+
+const createReferenceLabeledDescriptors = async (referenceImages) => {
+  const referenceImagesDescriptorTasks = await Promise.all(referenceImages.map(reference => faceapi.detectSingleFace(reference)
+    .withFaceLandmarks()
+    .withFaceDescriptor()));
+  return new faceapi.LabeledFaceDescriptors(referenceName, referenceImagesDescriptorTasks.map(descriptor => descriptor.descriptor));
+}
+
+const drawResults = (faceMatcher, queryDescriptors, queryImage) => {
+  const queryDrawBoxes = queryDescriptors.map(descriptors => {
+    const bestMatch = faceMatcher.findBestMatch(descriptors.descriptor);
+    return createDrawBox(descriptors, bestMatch);
+  })
+  const outQuery = faceapi.createCanvasFromMedia(queryImage);
+  queryDrawBoxes.forEach(drawBox => drawBox.draw(outQuery));
+  saveResultFile(outQuery)
+}
+
+const createDrawBox = (descriptors, bestMatch) => {
+  const boxColor = bestMatch.label === 'unknown' ? resultBox.unknownPersonColor : resultBox.matchColor;
+  return new faceapi.draw.DrawBox(descriptors.detection.box, {
+    label: bestMatch.label,
+    boxColor
+  });
+}
+
+const saveResultFile = async (resultFile) => {
+  const outputDir = await readdir(foldersPath.output);
+  const fileName = outputDir.length + 1;
+  writeFile(`${foldersPath.output}/${fileName}.jpg`, resultFile.toBuffer('image/jpeg'));
+}
+
+const main = async () => {
+  await initializeNeuralModels();
+  recognizeFaces();
+}
+
+main();
